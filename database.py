e93cd596f284806539d703097cfeafb8b6cfdd72
Saiyara <saiyaraislam17@gmail.com>
2026-01-12 13:35:51 -0500

    duckdb and sql queries added

--- database.py
+++ database.py
@@ -10,8 +10,43 @@ def create_database(database, table_name, fields):
     cur.close()
     conn.close()
 
-#def add_user(user_id, agent_id):
+def add_agent_and_thread(thread_id, agent_id):
+    conn = duckdb.connect("data/agents.db")
+    cur = conn.cursor()
+    cur.execute(f"INSERT INTO agent (agent_id, thread_id) VALUES ('{agent_id}', '{thread_id}')")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def add_thread_and_response(thread_id, response):
+    conn = duckdb.connect("data/threads.db")
+    cur = conn.cursor()
+    cur.execute(f"INSERT INTO thread (thread_id, response) VALUES ('{thread_id}', '{response}')")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def get_all_threads(agent_id):
+    conn = duckdb.connect("data/agents.db")
+    cur = conn.cursor()
+    df = cur.execute(f"select * from agent where agent_id='{agent_id}'").df()
+    cur.close()
+    conn.close()
+    return df
+
+def get_all_responses(thread_id):
+    conn = duckdb.connect("data/threads.db")
+    cur = conn.cursor()
+    df = cur.execute(f"select * from thread where thread_id='{thread_id}'").df()
+    cur.close()
+    conn.close()
+    return df
+
+#I assume the latest response is always at the bottom of the dataframe. just do df.tail(1)
 
 if __name__ == "__main__":
-    create_database("agents.db", "agent", ("agent_id", "thread_id"))
-    create_database("threads.db", "thread", ("thread_id", "response"))
+    #create_database("agents.db", "agent", ("agent_id", "thread_id"))
+    #create_database("threads.db", "thread", ("thread_id", "response"))
+    #add_agent_and_thread(thread_id="food", agent_id="some agent")
+    #add_thread_and_response(thread_id="hello", response="hewww")
+    print(get_all_responses(thread_id="hello"))
